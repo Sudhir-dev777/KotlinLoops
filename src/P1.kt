9a6e73f66f5b937beb72c3699ab51018750debdd
Jyoti Rani <ranijyoti392@gmail.com>
2017-09-18 00:48:07 +0530

    Added loop to print natural numbers.

---
+++ src/P1.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by jyotirani on 18/09/17.
+ */
+
+// Write a program to display the first ten natural numbers.
+
+fun main(args: Array<String>) {
+  for (number in 1..10) {
+    println(number)
+  }
+}
