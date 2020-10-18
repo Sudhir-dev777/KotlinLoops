7837c3022869a4f8466416c6650fc79feb82c02a
Sudhir-dev777 <73054428+Sudhir-dev777@users.noreply.github.com>
2020-10-18 17:47:40 +0530

    I am use array in your code.

--- src/P1.kt
+++ src/P1.kt
@@ -5,7 +5,16 @@
 // Write a program to display the first ten natural numbers.
 
 fun main(args: Array<String>) {
-  for (number in 1..10) {
-    println(number)
-  }
+
+    var language = arrayOf("Kotlin", "Java", "Python", "C++")
+
+    for (item in language.indices) {
+
+        
+        if (item%2 == 0)
+            println(language[item])
+    }
 }
+
+  
+  
